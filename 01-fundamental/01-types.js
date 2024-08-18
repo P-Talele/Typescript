9b3b4086e6431332b9657d38659affced0938497
prashant <prashanttalele1997@gmail.com>
2024-08-18 23:08:22 +0530

    initial commit

---
+++ 01-fundamental/01-types.js
@@ -0,0 +1,5 @@
+"use strict";
+// how to decelare variable and initilization
+var name = "Prashant";
+console.log(name);
+module.exports = {};
---
+++ 01-fundamental/01-types.ts
@@ -0,0 +1,7 @@
+ // how to decelare variable and initilization
+let name:String="Prashant";
+ console.log(name)
+
+
+
+ export={}  
