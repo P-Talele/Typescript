9727053d6f621afd46a06e1d9394b5c959b7714c
prashant <prashanttalele1997@gmail.com>
2024-08-19 16:33:49 +0530

    initial commit

--- 01-fundamental/02-function.js
+++ 01-fundamental/02-function.js
@@ -1,9 +1,37 @@
 "use strict";
-var status;
 function isLogged(status) {
-    return status = false;
+    if (status == true) {
+        return 'User Logged';
+    }
+    else {
+        return 'User Not Logged';
+    }
 }
-status = isLogged(true);
-console.log(status);
-console.log("this is logged function");
+var user = isLogged(false);
+console.log(user);
+function addition(first, second) {
+    return first + second;
+}
+var sum = addition(12, 23);
+console.log('Addition Of two Variable=' + sum);
+function goodMorning(name) {
+    var suffix = name ? ", ".concat(name, ".") : '.';
+    return 'Good Morning' + suffix;
+}
+console.log(goodMorning('Sam'));
+var arrowFunction = function (name, location) {
+    console.log('Name=' + name + ',Location=' + location);
+};
+var result = arrowFunction('Prashant', 'Pune');
+console.log(result);
+var stars = ['Virat', 'Rohit', 'Salman'];
+var resultStars = stars.map(function (item) {
+    return "".concat(item, " is heros");
+});
+console.log(resultStars);
+function noReturnType(name) {
+    console.log(name);
+}
+var result2 = noReturnType("Prashant");
+console.log(result2);
 module.exports = {};
--- 01-fundamental/02-function.ts
+++ 01-fundamental/02-function.ts
@@ -1,10 +1,40 @@
-let status:boolean
-function  isLogged(status:Boolean)
-{  
-     return status=false
+function isLogged (status: Boolean) {
+  if (status == true) {
+    return 'User Logged'
+  } else {
+    return 'User Not Logged'
+  }
 }
-  status =isLogged(true)
-console.log(status)
-console.log("this is logged function")
+let user: any = isLogged(false)
+console.log(user)
 
-export={}  
+function addition (first: number, second: number) {
+  return first + second
+}
+let sum: number = addition(12, 23)
+console.log('Addition Of two Variable=' + sum)
+
+function goodMorning (name: string): string {
+  const suffix = name ? `, ${name}.` : '.'
+  return 'Good Morning' + suffix
+}
+console.log(goodMorning('Sam'))
+const arrowFunction = (name: String, location: String) => {
+  console.log('Name=' + name + ',Location=' + location)
+}
+let result = arrowFunction('Prashant', 'Pune')
+console.log(result)
+
+const stars = ['Virat', 'Rohit', 'Salman']
+let resultStars = stars.map(item => {
+  return `${item} is heros`
+})
+console.log(resultStars)
+
+function noReturnType (name: String): void {
+  console.log(name)
+}
+let result2 = noReturnType('Prashant')
+console.log(result2)
+
+export = {}
